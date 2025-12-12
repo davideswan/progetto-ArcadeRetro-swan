48cffc783e87add3e38aacbba957cfe5992ca9f7
davide swan <d.swan@itsprodigi.it>
2025-12-12 14:30:14 +0100

    first commit

---
+++ src/models/Game.java
@@ -0,0 +1,46 @@
+package models;
+
+public class Game {
+	private String name;
+	private int releaseYear;
+	private int difficulty;
+	
+	public Game(String name, int releaseYear, int difficulty) {
+		super();
+		this.name = name;
+		this.releaseYear = releaseYear;
+		this.difficulty = difficulty;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getReleaseYear() {
+		return releaseYear;
+	}
+
+	public void setReleaseYear(int releaseYear) {
+		this.releaseYear = releaseYear;
+	}
+
+	public int getDifficulty() {
+		return difficulty;
+	}
+
+	public void setDifficulty(int difficulty) {
+		this.difficulty = difficulty;
+	}
+
+	@Override
+	public String toString() {
+		return name + " " + releaseYear + " " + difficulty;
+	}
+	
+	
+	
+}
